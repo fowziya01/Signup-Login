e987d8d0374424788f84cee1b444c134c1b40da8
fowziya01 <fowzijunaid@gmail.com>
2024-12-05 23:53:26 +0530

    feature/login

--- repo2.js
+++ repo2.js
@@ -8,5 +8,21 @@ function signUp(userName){
 arr.push(userName);
 return "Signup Successful, Please Login";
 }
-console.log(signUp("Alice"));
-console.log(signUp("Ziya"));
+//console.log(signUp("Alice"));
+//console.log(signUp("Ziya"));
+function login(userName1,password){
+    let arr1=["Alice","Julie","Bob","John"];
+for(let i=0;i<arr1.length;i++){
+    if(arr1[i]===userName1 && password==="Emp@123"){
+        return "Login Successful";
+    }
+}
+if(!arr1.includes(userName1)){
+    return  "User Not Found, Please Signup";
+} else {
+    return "Wrong Password";
+}
+}
+console.log(login("Alice", "Emp@123"));    // Output: Login Successful
+console.log(login("Bob", "WrongPass"));    // Output: Wrong Password
+console.log(login("Charlie", "Emp@123"));  
